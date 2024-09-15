5ce6a7b9577d45092e9caabf7094385efd056d65
Joshuad334 <Joshuad334@gmail.com>
2024-09-15 16:28:43 -0400

    Update Python Table Population Script.py

--- Python Table Population Script.py
+++ Python Table Population Script.py
@@ -15,7 +15,7 @@ records = df_data.values.tolist()
 
 # Step 4. SQL Ingestion - Ingests the Data into SQL Server
 Driver = "ODBC Driver 17 for SQL Server"
-Server_Name = "DESKTOP-4COA7Q3\\SQLEXPRESS" #Fake Server Name for obvious Reasons
+Server_Name = "DESKTOP-4COA7Q3\\SQLEXPRESS" # Fake Server Name for obvious reasons
 Database_Name = "LogIstics" # Fake Database Name for obvious reasons
 
 def connection_string(Driver, Server_Name, Database_Name):
@@ -37,8 +37,12 @@ except odbc.Error as e:
 
 try:
     cursor = conn.cursor()
+    insert_query = """
+    INSERT INTO [Logistics].[dbo].[Customer] (C_ID, M_ID, C_NAME, C_EMAIL_ID, C_TYPE, C_ADDR, C_CONT_NO)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    """
     for record in records:
-        cursor.execute(records)
+        cursor.execute(insert_query, record)
     conn.commit()
 except Exception as e:
     conn.rollback()
